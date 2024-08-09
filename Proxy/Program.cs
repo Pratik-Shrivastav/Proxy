c823ef57020d2bd17c0465da8466fe11d8f6d099
Pratik.Shrivastav <Pratik.Shrivastav@DELL1615>
2024-08-09 12:32:01 +0530

    Add project files.

---
+++ Proxy/Program.cs
@@ -0,0 +1,27 @@
+﻿using Proxy.Model;
+using Proxy.Service;
+
+namespace Proxy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Anurag", "Anurag123", "Developer");
+            SharedProxy folderProxy1 = new SharedProxy(emp1);
+            folderProxy1.RWOperations();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Employee emp2 = new Employee("Pranaya", "Pranaya123", "Manager");
+            SharedProxy folderProxy2 = new SharedProxy(emp2);
+            folderProxy2.RWOperations();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Employee emp3 = new Employee("Pratik", "Pratik123", "CEO");
+            SharedProxy folderProxy3 = new SharedProxy(emp3);
+            folderProxy3.RWOperations();
+        }
+    }
+}
---
+++ Proxy/Service/SharedProxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proxy.Model;
+
+namespace Proxy.Service
+{
+    public class SharedProxy : ISharedFolder
+    {
+        private ISharedFolder folder;
+        private Employee employee;
+        public SharedProxy(Employee emp)
+        {
+            employee = emp;
+        }
+        public void RWOperations()
+        {
+            if (employee.Role.ToUpper() == "CEO" || employee.Role.ToUpper() == "MANAGER")
+            {
+                folder = new SharedFolder();
+                Console.WriteLine("Shared Folder Proxy makes call to the RealFolder 'PerformRWOperations method'");
+                folder.RWOperations();
+            }
+            else
+            {
+                Console.WriteLine("Shared Folder proxy says 'You don't have permission to access this folder'");
+            }
+        }
+    }
+}
+
